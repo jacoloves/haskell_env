40c0623ee10ad7f831a454296c12787a9f0eadaf
jacoloves <5511068t@gmail.com>
2023-06-22 08:27:26 +0900

    chap5 add

---
+++ h_haskell/chap5/chap5.hs
@@ -0,0 +1,52 @@
+multThree :: Int -> (Int -> (Int -> Int))
+multThree x y z = x * y * z
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = filter (<= x) xs
+        larger = filter (> x) xs
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n = n : chain (n * 3 + 1)
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+sum'2 :: (Num a) =? [a] -> a
+sum'2 = foldl  (+) 0
+
+oddSquareSum :: Integer
+-- oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+oddSquareSum = sum . takeWhile (<10000) . filter odd $ map (^2) [1..]
