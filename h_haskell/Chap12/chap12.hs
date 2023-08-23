03404b96dec03dfa9e8086d1fcddc75a17607950
jacoloves <5511068t@gmail.com>
2023-08-24 04:42:28 +0900

    chap12 add

---
+++ h_haskell/Chap12/chap12.hs
@@ -0,0 +1,20 @@
+import Distribution.Simple.Build (componentInitialBuildSteps)
+-- CharList
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq, Show)
+
+-- Pair
+newtype Pair b a = Pair { getPair :: (a, b) }
+
+-- fmap
+instance Functor (Pair c) where
+  fmap f (Pair (x, y)) = Pair (f x, y)
+
+-- CoolBool
+-- data CoolBool = CoolBool { getCoolBool :: Bool }
+newtype CoolBool = CoolBool { getCoolBool :: Bool }
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
+
+-- IntList
+type IntList = [Int]
