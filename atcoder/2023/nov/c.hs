afee34a7f486a5419d4a634c1cfbc4db051a123f
jacoloves <5511068t@gmail.com>
2023-11-03 23:07:24 +0900

    20231103

--- atcoder/2023/nov/c.hs
+++ atcoder/2023/nov/c.hs
@@ -2,15 +2,6 @@ main :: IO ()
 main = do
     [a, b] <- map read . words <$> getLine
 
-    let ans = f a b
-
-    print ans
-
-
-f :: Int -> Int -> String
-f a b
-    | if a == b then No
-      else if a > b && b + 3 >= a then Yes
-      else if a < b && a + 2 >= b then Yes
-      otherwise  No
+    if (a < b && a + 2 >= b) || (a > b && b + 3 >= a) then putStrLn "Yes"
+    else  putStrLn "No"
 
